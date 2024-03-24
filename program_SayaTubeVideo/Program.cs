9e33287c63c915e1113c3729bd600883e96a9790
Syahreza Adnan Al Azhar <notsupersyahreza@student.telkomuniversity.ac.id>
2024-03-25 01:36:50 +0700

    menambahkan class SayaTubeVideo

--- program_SayaTubeVideo/Program.cs
+++ program_SayaTubeVideo/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        this.id = random.Next(10000, 100000);
+        this.title = title;
+        this.playCount = 0;
+    }
+    public void IncreasePlayCount(int playCount)
+    {
+        this.playCount += playCount;
+    }
+    public void PrintVideoDetail()
+    {
+        Console.WriteLine("Informasi Video");
+        Console.WriteLine($"ID         : {this.id}");
+        Console.WriteLine($"Title      : {this.title}");
+        Console.WriteLine($"Play Count : {this.playCount}");
+    }
+}
+
+public class main
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo STV = new SayaTubeVideo("Tutorial Design By Contract - SYAHREZA ADNAN AL AZHAR");
+        STV.IncreasePlayCount(10000001);
+        STV.PrintVideoDetail();
+    }
+}
